da0da56f3081fc19bf7539b5c92d728f478079d9
Antheny Wells <anthenywells@go-away.lan>
2020-06-19 14:06:12 -0400

    category update, delete, list all

--- controllers/category.js
+++ controllers/category.js
@@ -5,7 +5,7 @@ exports.categoryById = (req, res, next, id) => {
   Category.findById(id).exec((err, category) => {
     if (err || !category) {
       return res.status(400).json({
-        error: "Category does not exist."
+        error: "Category does not exist.",
       });
     }
     req.category = category;
@@ -27,5 +27,46 @@ exports.create = (req, res) => {
 };
 
 exports.read = (req, res) => {
-  return res.json(req.category)
+  return res.json(req.category);
+};
+
+exports.update = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+
+  category.save((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+
+    res.json(data);
+  });
+};
+
+exports.remove = (req, res) => {
+  const category = req.category;
+
+  category.remove((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+
+    res.json({
+      message: "Category successfully deleted.",
+    });
+  });
+};
+exports.list = (req, res) => {
+  Category.find().exec((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json(data);
+  });
 };
