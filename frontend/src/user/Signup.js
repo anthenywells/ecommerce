427893d80c13e5628030ecb09d434efe255d1967
Antheny Wells <anthenywells@go-away.lan>
2020-06-22 20:38:10 -0400

    pages + layout

---
+++ frontend/src/user/Signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../core/Layout";
+
+const Signup = () => (
+  <Layout
+    title="Signup"
+    description="Signup Node React E-commerce App"
+  ></Layout>
+);
+
+export default Signup;
