1f22243c9fc46ae914a8d4042dd1b7e12d93704b
Antheny Wells <anthenywells@Go-away.local>
2020-06-26 14:06:29 -0400

    create product

--- frontend/src/admin/apiAdmin.js
+++ frontend/src/admin/apiAdmin.js
@@ -6,7 +6,7 @@ export const createCategory = (userId, token, category) => {
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(category),
   })
@@ -15,5 +15,34 @@ export const createCategory = (userId, token, category) => {
     })
     .catch((err) => {
       console.log("signup -> err", err);
+    });
+};
+
+export const createProduct = (userId, token, product) => {
+  return fetch(`${API}/product/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: product,
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.log("signup -> err", err);
+    });
+};
+
+export const getCategories = () => {
+  return fetch(`${API}/categories`, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.log("getCategories -> err", err);
     });
 };
