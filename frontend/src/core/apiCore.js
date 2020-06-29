0370c115e2dd1141a327af2702505d1906def82d
Antheny Wells <anthenywells@go-away.lan>
2020-06-28 22:28:11 -0400

    sort + filter + load more

--- frontend/src/core/apiCore.js
+++ frontend/src/core/apiCore.js
@@ -11,3 +11,37 @@ export const getProducts = (sortBy) => {
       console.log("getProducts -> err", err);
     });
 };
+
+export const getCategories = () => {
+  return fetch(`${API}/categories`, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.log("getCategories -> err", err);
+    });
+};
+
+export const getFilteredProducts = (skip, limit, filters = {}) => {
+  const data = {
+      limit,
+      skip,
+      filters
+  };
+  return fetch(`${API}/products/by/search`, {
+      method: "POST",
+      headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+  })
+      .then(response => {
+          return response.json();
+      })
+      .catch(err => {
+          console.log(err);
+      });
+};
