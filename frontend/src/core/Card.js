d5cb747c124dbb0bb83c3799b3af3ed4be3f6e3c
Antheny Wells <anthenywells@go-away.lan>
2020-07-17 16:35:36 -0400

    add to cart + cart nav link

--- frontend/src/core/Card.js
+++ frontend/src/core/Card.js
@@ -1,10 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
 import ProductPhoto from "./ProductPhoto";
 import moment from "moment";
+import { addItem } from "./cartHelpers";
 
 const Card = ({ product, showViewProductButton = true }) => {
-  const { name, description, price, quantity, category, createdAt, _id } = product;
+  const [redirect, setRedirect] = useState(false);
+
+  const {
+    name,
+    description,
+    price,
+    quantity,
+    category,
+    createdAt,
+    _id,
+  } = product;
 
   const showStock = (quantity) => {
     return quantity > 0 ? (
@@ -14,29 +25,49 @@ const Card = ({ product, showViewProductButton = true }) => {
     );
   };
 
-  const showViewButton = showViewProductButton => {
+  const showViewButton = (showViewProductButton) => {
     return (
       showViewProductButton && (
         <Link to={`/product/${_id}`} className="mr-2">
-          <button className="btn btn-outline-primary mt-2 mb-2 card-btn-1">View Product</button>
+          <button className="btn btn-outline-primary mt-2 mb-2 card-btn-1">
+            View Product
+          </button>
         </Link>
       )
     );
   };
 
-  const showAddToCartButton = () => (
-    <button className="btn btn-outline-warning col-6">Add to Cart</button>
-  );
+  const addToCart = () => {
+    addItem(product, () => {
+      setRedirect(true);
+    });
+  };
+
+  const shouldRedirect = (redirect) => {
+    if (redirect) {
+      return <Redirect to="/cart" />;
+    }
+  };
+
+  const showAddToCartButton = () => {
+    return (
+      <button onClick={addToCart} className="btn btn-outline-warning col-6">
+        Add to Cart
+      </button>
+    );
+  };
   return (
     <div className="card">
       <div className="card-header name">{name}</div>
       <div className="card-body">
+        {shouldRedirect(redirect)}
         <ProductPhoto item={product} url={"product"} />
         <p className="lead">{description}</p>
         <p className="black-10">${price}</p>
         <p className="black-9">Category: {category && category.name}</p>
         <p className="black-8">Added on: {moment(createdAt).fromNow()}</p>
-        {showStock(quantity)}<br/>
+        {showStock(quantity)}
+        <br />
         {showViewButton(showViewProductButton)}
         {showAddToCartButton()}
       </div>
