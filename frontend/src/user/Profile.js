9dd0f599ce881abd47b3947b30e2d51aa2aa5d4b
Antheny Wells <anthenywells@go-away.lan>
2020-07-24 12:48:12 -0400

    get user info for profile update

--- frontend/src/user/Profile.js
+++ frontend/src/user/Profile.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import { read, update, updateUser } from "./apiUser";
 
-const Profile = () => {
+const Profile = ({ match }) => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+
+  const { name, email, password, error, success } = values;
+  const { token } = isAuthenticated();
+
+  const init = (userId) => {
+    read(userId, token).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: true });
+      } else {
+        setValues({
+          ...values,
+          name: data.name,
+          email: data.email,
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    init(match.params.userId);
+  }, []);
+
   return (
-    <h2>Profile</h2>
-  )
-}
+    <Layout title="Profile" description="Update Your Profile">
+      <h2 className="mb-4">Profile Update</h2>
+      {JSON.stringify(values)}
+    </Layout>
+  );
+};
 
 export default Profile;
