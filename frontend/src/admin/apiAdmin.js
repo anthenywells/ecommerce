e1d3c05f3fb89014e1a2a0b808658b208aa92a4f
Antheny Wells <anthenywells@go-away.lan>
2020-07-24 16:57:00 -0400

    product CRUD requests

--- frontend/src/admin/apiAdmin.js
+++ frontend/src/admin/apiAdmin.js
@@ -96,3 +96,68 @@ export const updateOrderStatus = (userId, token, orderId, status) => {
       console.log("updateOrderStatus -> err", err);
     });
 };
+
+//to perform CRUD on products:
+// get all products
+// get single product
+// update single product
+// delete single product
+
+export const getProducts = () => {
+  return fetch(`${API}/products`, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.log("getProducts -> err", err);
+    });
+};
+
+export const getProduct = (productId) => {
+  return fetch(`${API}/product/${productId}`, {
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.log("getProducts -> err", err);
+    });
+};
+
+export const updateProduct = (productId, userId, token, product) => {
+  return fetch(`${API}/product/${productId}/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: product,
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.log("updateProduct -> err", err);
+    });
+};
+
+export const deleteProduct = (productId, userId, token) => {
+  return fetch(`${API}/product/${productId}/${userId}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.log("deleteProduct -> err", err);
+    });
+};
